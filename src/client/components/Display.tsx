21c313d0eee8ca726bd00c54ce29396fc9365047
Paula <pauladzuck@hotmail.com>
2020-08-17 06:27:55 -0500

    Adding updates 6:30 am

--- src/client/components/Display.tsx
+++ src/client/components/Display.tsx
@@ -1,36 +1,38 @@
 import * as React from 'react';
-import {useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import moment from 'moment';
-import {Link, useHistory} from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { IBook } from '../utils/interfaces';
 
-const Display: React.FC<DisplayProps> = ({book}) => {
+const Display: React.FC<DisplayProps> = ({ book }) => {
 
     const history = useHistory();
 
 
     return (
 
-        <div className="col-md-5 mx-4"onClick={() => history.push(`/details/${book.id}`)}>
-        <div className= "mx-2"> {book.title}</div>
-        <div>
-            <div className="mx-2">
-                <h6>{book.author}</h6>
+        <section className="row justify-content-center col-mx-3">
+            <div className="my-3" onClick={() => history.push(`/details/${book.id}`)}>
+                <div className="mx-2"> {book.title}</div>
+                <div>
+                    <div className="mx-2">
+                        <h6>{book.author}</h6>
+                    </div>
+                    <div className="mx-2">
+                        <h6>{book.name}</h6>
+                    </div>
+                    <div className="mx-2">
+                        <h6>${book.price}</h6>
+                    </div>
+                </div>
             </div>
-            <div  className="mx-2">
-                <h6>{book.name}</h6>
-            </div>
-            <div  className="mx-2">
-                <h6>${book.price}</h6>
-            </div>
-        </div>
-    </div>
+            </section>
 
     );
 }
 
 interface DisplayProps {
-    book: IBook
+                book: IBook
 }
 
 export default Display;
