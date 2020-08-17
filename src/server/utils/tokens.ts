8a737d6045b37f34d56e604dc85291fd54498283
Paula <pauladzuck@hotmail.com>
2020-08-17 02:29:14 -0500

    Adding updates

---
+++ src/server/utils/tokens.ts
@@ -0,0 +1,13 @@
+import * as jwt from 'jsonwebtoken';
+import config from '../config';
+import type { IPayload } from './types';
+
+export const createToken = (payload: IPayload) => {
+
+    const token = jwt.sign(payload, config.auth.secret);
+    return token;
+
+
+
+}
+
